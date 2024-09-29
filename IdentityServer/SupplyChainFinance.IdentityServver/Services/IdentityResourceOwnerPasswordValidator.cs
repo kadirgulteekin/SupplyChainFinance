8abf643af76586ee7dee5df7ba9b42317142529a
Kadir Gultekin <kadirgulteekin@gmail.com>
2024-09-30 00:21:00 +0300

    IdentityResourceOwnerPasswordValidator

---
+++ IdentityServer/SupplyChainFinance.IdentityServver/Services/IdentityResourceOwnerPasswordValidator.cs
@@ -0,0 +1,50 @@
+﻿using IdentityModel;
+using IdentityServer4.Validation;
+using Microsoft.AspNetCore.Identity;
+using SupplyChainFinance.IdentityServver.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SupplyChainFinance.IdentityServver.Services
+{
+
+    public class IdentityResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public IdentityResourceOwnerPasswordValidator(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            var IsExist = await _userManager.FindByEmailAsync(context.UserName);
+
+            if (IsExist == null)
+            {
+                var errors = new Dictionary<string, object>();
+                errors.Add("errors", new List<string> { "Email veya şifreniz yanlış" });
+                context.Result.CustomResponse = errors;
+
+                return;
+            }
+
+            var passwordCheck = await _userManager.CheckPasswordAsync(IsExist, context.Password);
+
+            if (passwordCheck == false)
+            {
+                var errors = new Dictionary<string, object>();
+                errors.Add("errors", new List<string> { "Email veya şifreniz yanlış" });
+                context.Result.CustomResponse = errors;
+
+                return;
+            }
+
+            context.Result = new GrantValidationResult(IsExist.Id.ToString(), OidcConstants.AuthenticationMethods.Password);
+        }
+
+
+    }
+}
+
